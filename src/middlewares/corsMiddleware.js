9ee7c0d1088ba4223163853eb4db58c3ecf90cae
Ignacio Tello <ignacio.tello.dev@gmail.com>
2024-12-07 14:55:18 -0300

    se están corrigiendo los errores de CORS

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -2,6 +2,6 @@ import cors from 'cors'
 
 export const corsMiddleware = cors({
     origin:`https://sch-web.vercel.app`, 
-    methods:['GET', 'POST'],
+    methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
 })
