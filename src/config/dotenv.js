2e5702ad6394b50212a70a0863a255546aadb5c7
Ignacio Tello <ignacio.tello.dev@gmail.com>
2024-12-07 14:30:48 -0300

    sacando data sensible

--- src/config/dotenv.js
+++ src/config/dotenv.js
@@ -5,5 +5,8 @@ dotenv.config({path:"./src/.env", override:true})
 
 export const config = {
     SECRETKEY:process.env.SECRETKEY,
-    PORT:process.env.PORT || 3000
+    PORT:process.env.PORT || 3000,
+    DEV_EMAIL:process.env.DEV_EMAIL,
+    CLIENT_EMAIL:process.env.CLIENT_EMAIL,
+    PASSMAILER:process.env.PASSMAILER
 }
--- src/config/nodemailer.config.js
+++ src/config/nodemailer.config.js
@@ -1,19 +1,20 @@
 import nodemailer from 'nodemailer'
+import { config } from 'dotenv.js'
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
     port: 587,
     auth: {
-      user: "ignacio.tello.dev@gmail.com",
-      pass: "unmo hhna iygg phtr",
+      user: config.DEV_EMAIL,
+      pass: config.PASSMAILER,
     },
   })
 
   export const enviarMail=async(message)=>{
     return await transporter.sendMail(
         {
-            from:"schDev ignacio.tello.dev@gmail.com",
-            to:'ignacio.tello.dev@gmail.com', 
+            from:`schDev ${config.DEV_EMAIL}`,
+            to:config.DEV_EMAIL, 
             html: message
         }
     )
