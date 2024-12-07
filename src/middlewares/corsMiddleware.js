7b453b3ccd0c6b69f435c52834e8480d4884ada3
Ignacio Tello <ignacio.tello.dev@gmail.com>
2024-12-07 00:57:04 -0300

    corrigiendo formulario de contacto

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,7 +1,7 @@
 import cors from 'cors'
 
 export const corsMiddleware = cors({
-    origin:`https://sch-web.vercel.app/`, 
+    origin:`https://sch-web.vercel.app`, 
     methods:['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 })
